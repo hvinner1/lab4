3ab6d8f6f275eed957a1057079021a0ddaa38b51
hvinner <91498701+hvinner1@users.noreply.github.com>
2021-10-19 22:40:12 +0000

    everything

---
+++ background.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python37all
+# This code runs continually in the background to apply
+# the stored PWM slider value to the GPIO output
+import RPi.GPIO as GPIO
+import time
+import json
+
+
+green = 19
+yellow = 13
+red = 6
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(green, GPIO.OUT)
+GPIO.setup(yellow, GPIO.OUT)
+GPIO.setup(red, GPIO.OUT)
+
+
+#json
+with open('lab4.txt'
+, 'r') as f:
+  data = json.load(f)
+print("button = " + str(data['option']))
+print("slider 1 = " + str(data['slider1']))
+
+
+
+pwm1 = GPIO.PWM(green, 100) # PWM object on our pin at 100 Hz
+pwm1.start(0) # start with LED off
+while True:
+  with open("lab4.txt", 'r') as f:
+    dutyCycle = float(f.read()) # read duty cycle value from file
+  pwm1.ChangeDutyCycle(dutyCycle)
+  time.sleep(0.1)
+pwm2 = GPIO.PWM(yellow, 100) # PWM object on our pin at 100 Hz
+pwm2.start(0) # start with LED off
+while True:
+  with open("lab4.txt", 'r') as f:
+    dutyCycle = float(f.read()) # read duty cycle value from file
+  pwm2.ChangeDutyCycle(dutyCycle)
+  time.sleep(0.1)
+pwm3 = GPIO.PWM(red, 100) # PWM object on our pin at 100 Hz
+pwm3.start(0) # start with LED off
+while True:
+  with open("lab4.txt", 'r') as f:
+    dutyCycle = float(f.read()) # read duty cycle value from file
+  pwm3.ChangeDutyCycle(dutyCycle)
+  time.sleep(0.1)
+
---
+++ lab4.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python37all
+import cgi
+import json
+
+data = cgi.FieldStorage()
+
+#json code
+b1 = data.getvalue('option')
+s1 = data.getvalue('slider1')
+data = {"option":b1,"slider1":s1}
+with open('lab4.txt', 'w') as f:
+  json.dump(data,f)
+
+print('Content-type: text/html\n\n')
+print('''
+<html>
+<form action="/cgi-bin/lab4.py" method="POST">
+  <input type="radio" name="option" value="1" checked> LED 1 <br>
+  <input type="radio" name="option" value="2"> LED 2 <br>
+  <input type="radio" name="option" value="3"> LED 3 <br>
+  <input type="submit" value="Submit">
+''')
+print('<input type="range" name="slider1" min="0" max="100" value="%s"><br>' % s1)
+print('<input type="submit" value=”Change LED brightness">')
+print('</form>')
+print('Brightness = %s' % s1)
+print('</html>')
